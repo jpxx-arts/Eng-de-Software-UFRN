c0fdbd8939bef0432d683b0603e12bdc5aa449da
jpxx-arts <jpramalho123@hotmail.com>
2025-11-01 16:31:53 -0300

    feat: first structure of the backend project with MVC architecture pattern

---
+++ tomaladaka/src/main/java/com/umbrella/tomaladaka/TomaladakaApplication.java
@@ -0,0 +1,13 @@
+package com.umbrella.tomaladaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomaladakaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TomaladakaApplication.class, args);
+  }
+
+}
---
+++ tomaladaka/src/test/java/com/umbrella/tomaladaka/TomaladakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbrella.tomaladaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomaladakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
